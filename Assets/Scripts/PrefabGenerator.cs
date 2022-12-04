3b8a75299e4d66d17c2ade2be746fbe3dd9479dd
safasifa <sofianm.visualstudio@gmail.com>
2022-12-03 22:03:45 -0500

    Adding Spawner
    Added spawn areas (as rect transforms), and prefab generator

--- Assets/Scripts/PrefabGenerator.cs
+++ Assets/Scripts/PrefabGenerator.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class PrefabGenerator : MonoBehaviour
 {
+  
+    [Header("In the same order as the floats below")]
+    public GameObject[] objectsToSpawn;
+    [Space]
+    public int blobCount;
+    public int enemyCount;
+    public int foodCount;
+    [Space]
+    public GameObject[] spawnArea;
+    private int[] countList;
     // Start is called before the first frame update
     void Start()
     {
-        
+        FillFloatArray();
+        CreatePool();
+
+    }
+    void FillFloatArray ()
+    {
+        countList = new int[3];
+        countList[0] = blobCount;
+        countList[1] = enemyCount;
+        countList[2] = foodCount;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void CreatePool()
     {
-        
+        for (int a = 0; a < 3; a++)
+        {
+            for (int i = 0; i < countList[a]; i++)
+            {
+                PlaceObj(a);
+            }
+        }
+    }
+
+    void PlaceObj(int a)
+    {
+        Debug.Log("aaa");
+        bool stopLoop = false;
+        RectTransform area = spawnArea[a].GetComponent<RectTransform>();
+        for (int i = 0; stopLoop == false; i++) {
+            Vector3 spawnPos = area.transform.position+ new Vector3(Random.Range(area.rect.xMax, area.rect.xMin), Random.Range(area.rect.yMin, area.rect.yMax));
+            Debug.Log("Colliders"+ Physics2D.OverlapCircle(spawnPos, 3));
+            if (Physics2D.OverlapCircle(spawnPos, 3) == null)
+            {
+                Debug.Log("overlapisfalse");
+                Instantiate(objectsToSpawn[a], spawnPos, Quaternion.identity);
+                stopLoop = true;
+            }
+        }
+
     }
 }
