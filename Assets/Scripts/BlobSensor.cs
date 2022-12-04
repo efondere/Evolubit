c44241218ed1423e7e2d9f84fec2891e4691342c
efondere <ef@eliotfondere.com>
2022-12-04 14:32:19 -0500

    Refactor and Improve Blob

---
+++ Assets/Scripts/BlobSensor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobSensor : MonoBehaviour
+{
+    private BlobBehavior m_parentBehavior;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_parentBehavior = transform.parent.GetComponent<BlobBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        m_parentBehavior.OnSensorTriggerEnter(collision);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        m_parentBehavior.OnSensorTriggerStay(collision);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        m_parentBehavior.OnSensorTriggerExit(collision);
+    }
+}
--- Assets/Scripts/Managers/GenomeManager.cs
+++ Assets/Scripts/Managers/GenomeManager.cs
@@ -3,11 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
-using Random = Unity.Mathematics.Random;
 
+/*
 public class GenomeData
 {
-    const int GENOME_SIZE = 50;
+    const int GENOME_SIZE = 20;
     const int CODON_SIZE = 13;
     const int MAX_LIFE = 10;
     const bool MUTATIONS_ENABLED = false;
@@ -135,10 +135,12 @@ public class GenomeData
                 if (parameterInt < 6)
                 {
                     weight -= (float)bitArrayToInt(value) / Mathf.Pow(2, CODON_SIZE - 2 - 5);
+                    if (weight < 0.0f) weight = 0.0f;
                 }
                 else if (parameterInt < 12)
                 {
                     strength -= (float)bitArrayToInt(value) / Mathf.Pow(2, CODON_SIZE - 2 - 5);
+                    if (strength < 0.0f) strength = 0.0f;
                 }
                 else if (parameterInt < 18)
                 {
@@ -151,10 +153,12 @@ public class GenomeData
                 else if (parameterInt < 24)
                 {
                     senseStrength -= (float)bitArrayToInt(value) / Mathf.Pow(2, CODON_SIZE - 2 - 5);
+                    if (senseStrength < 0.12f) senseStrength = 0.12f;
                 }
                 else if (parameterInt < 32)
                 {
                     lifetime = (uint)(((float)lifetime / MAX_LIFE - (float)bitArrayToInt(value) / Mathf.Pow(2, CODON_SIZE - 2 - 5)) * MAX_LIFE);
+                    if (lifetime < 1) lifetime = 1;
                 }
             }
             if (opInt == 3) // multiply
@@ -208,6 +212,102 @@ public class GenomeData
     {
         throw new NotImplementedException();
     }
+}*/
+
+class GenomeData
+{
+    public float[] weight = new float[2];
+    public float[] strength = new float[2];
+    public float[] senseStrength = new float[2];
+    public Color[] color = new Color[2];
+    public uint[] lifetime = new uint[2];
+
+    public GenomeData()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            weight[i] = UnityEngine.Random.Range(0.1f, 1.0f);
+            strength[i] = UnityEngine.Random.Range(0.1f, 1.0f);
+            senseStrength[i] = UnityEngine.Random.Range(0.1f, 1.0f);
+            color[i] = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
+            lifetime[i] = (uint)Mathf.Max(UnityEngine.Random.value * 5.0f, 1.0f);
+        }
+    }
+
+    public static GenomeData Reproduce(GenomeData parent1, GenomeData parent2)
+    {
+        GenomeData data = new GenomeData();
+        data.weight[0] = parent1.weight[UnityEngine.Random.Range(0, 2)];
+        data.weight[1] = parent2.weight[UnityEngine.Random.Range(0, 2)];
+        data.strength[0] = parent1.strength[UnityEngine.Random.Range(0, 2)];
+        data.strength[1] = parent2.strength[UnityEngine.Random.Range(0, 2)];
+        data.senseStrength[0] = parent1.senseStrength[UnityEngine.Random.Range(0, 2)];
+        data.senseStrength[1] = parent2.senseStrength[UnityEngine.Random.Range(0, 2)];
+        data.color[0] = parent1.color[UnityEngine.Random.Range(0, 2)];
+        data.color[1] = parent2.color[UnityEngine.Random.Range(0, 2)];
+        data.lifetime[0] = parent1.lifetime[UnityEngine.Random.Range(0, 2)];
+        data.lifetime[1] = parent2.lifetime[UnityEngine.Random.Range(0, 2)];
+        data.applyMutations();
+        return data;
+    }
+
+    private float randomMutationValue()
+    {
+        if (UnityEngine.Random.value >= 0.05f)
+        {
+            return 0.0f;
+        }
+
+        float a = 0.05f;
+        float k = -a / 2.0f;
+        float c = 1.0f;
+        float x = UnityEngine.Random.Range(-1.0f, 1.0f);
+
+        return (a / (1 + Mathf.Exp(-c * x))) + k;
+    }
+
+    private void applyMutations()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            weight[i] += randomMutationValue();
+            strength[i] += randomMutationValue();
+            senseStrength[i] += randomMutationValue();
+            color[i] += new Color(randomMutationValue(), randomMutationValue(), randomMutationValue());
+            if (UnityEngine.Random.value < 0.05)
+            {
+                int change = UnityEngine.Random.Range(-1, 2);
+                if (change == -1) lifetime[i]--;;
+                if (change == 1) lifetime[i]++;;
+            }
+        }
+    }
+
+    public float getWeight()
+    {
+        return (weight[0] + weight[1]) / 2.0f;
+    }
+    public float getStrength()
+    {
+        return (strength[0] + strength[1]) / 2.0f;
+    }
+    public float getSenseStength()
+    {
+        return (senseStrength[0] + senseStrength[1]) / 2.0f;
+    }
+    public Color getColor()
+    {
+        Vector3 color1 = new Vector3(color[0].r, color[0].g, color[0].b);
+        Vector3 color2 = new Vector3(color[1].r, color[1].g, color[1].b);
+        Vector3 average = color1 + color2;
+        average /= 2.0f;
+
+        return new Color(Mathf.Min(average.x, 1.0f), Mathf.Min(average.y, 1.0f), Mathf.Min(average.z, 1.0f));
+    }
+    public uint getLifetime()
+    {
+        return (uint)Mathf.CeilToInt((lifetime[0] + lifetime[1]) / 2.0f);
+    }
 }
 
 public class GenomeManager : MonoBehaviour
@@ -218,7 +318,7 @@ public class GenomeManager : MonoBehaviour
         m_genomeData = new GenomeData();
     }
 
-    void Setup(GenomeData parent1, GenomeData parent2)
+    void CreateOffspring(GenomeData parent1, GenomeData parent2)
     {
         m_genomeData = GenomeData.Reproduce(parent1, parent2);
     }
@@ -231,23 +331,22 @@ public class GenomeManager : MonoBehaviour
 
     public float getSightRadius()
     {
-        return Mathf.Min(m_genomeData.senseStrength * 5, 1.0f);
+        return m_genomeData.getSenseStength() * 5;
     }
 
     public float getSpeed()
     {
-        // find better function
-        return Mathf.Max(m_genomeData.strength / m_genomeData.weight, 2.0f);
+        return (((Mathf.Pow(m_genomeData.getStrength(), 2.0f) - Mathf.Pow(m_genomeData.getWeight(), 2.0f)) / 2.0f) + 0.5f) * 10.0f;
     }
 
     public float getMovementFactor()
     {
-        return ((m_genomeData.senseStrength + m_genomeData.weight) / 2.0f) * 50.0f;
+        return ((m_genomeData.getSenseStength() + m_genomeData.getWeight()) / 2.0f) * 50.0f / getSpeed();
     }
 
     public Color getColor()
     {
-        return m_genomeData.color;
+        return m_genomeData.getColor();
     }
 
     private GenomeData m_genomeData;
